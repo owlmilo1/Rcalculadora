80a385919b69c5bc1844306e121e010e9fc6bfb6
Miguel Angelo <angelomig13@gmail.com>
2021-02-19 17:38:19 -0300

    first commit

---
+++ R/calculadora.R
@@ -0,0 +1,59 @@
+#' Function to sum
+#'
+#' This function is good to sum
+#'
+#' @param a A number
+#' @param b It is another number
+#'
+#' @examples
+#' somar(3,2)
+#'
+#' @export
+somar <- function(a,b) {
+  a + b
+}
+
+#' Function to subtract
+#'
+#' This function is good to subtract
+#'
+#' @param a A number
+#' @param b It is another number
+#'
+#' @examples
+#' subtrair(3,2)
+#'
+#' @export
+subtrair <- function(a,b) {
+  a - b
+}
+
+#' Function to multiply
+#'
+#' This function is good to multiply
+#'
+#' @param a A number
+#' @param b It is another number
+#'
+#' @examples
+#' multiplicar(3,2)
+#'
+#' @export
+multiplicar <- function(a,b) {
+  a * b
+}
+
+#' Function to divide
+#'
+#' This function is good to divide
+#'
+#' @param a A number
+#' @param b It is another number
+#'
+#' @examples
+#' dividir(3,2)
+#'
+#' @export
+dividir <- function(a,b) {
+  a / b
+}
